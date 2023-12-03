0756a3d69fcaa014bc54273bac81cd73f3236bec
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-03 22:18:00 +0300

    finishing hero section adding hero image and overlay

--- carshowcase/components/Hero.tsx
+++ carshowcase/components/Hero.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import CustomButton from "./CustomButton";
 
 const Hero = () => {
@@ -21,6 +22,14 @@ const Hero = () => {
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
+
+        <div className="hero__image-container">
+          <div className="hero__image">
+            <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          </div>
+
+          <div className="hero__image-overlay" />
+        </div>
       </div>
     </div>
   );
