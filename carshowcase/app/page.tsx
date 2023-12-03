a6d12d638a4f7165e51bb84b78a9c415466c3c97
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-03 22:12:24 +0300

    adding custom button and hero section

--- carshowcase/app/page.tsx
+++ carshowcase/app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
-  return <main className="">Hello</main>;
+  return (
+    <main className="">
+      <Hero />
+    </main>
+  );
 }
